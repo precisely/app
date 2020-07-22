29f97884e551f3b42ed417f32e7fc4b80c105b14
Constantine Vetoshev <constantine@precise.ly>
2020-07-21 23:23:39 -0700

    Begin change to function-based components.

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,34 +1,59 @@
 import * as React from "react";
+import * as RouterDOM from "react-router-dom";
+
+import * as AuthUtils from "~/src/utils/auth";
+import { Button } from "~/src/components/Button";
+import { Logout } from "~/src/components/Logout";
 
 import logoFileLt from "~/assets/images/logo/full-red-black.svg";
 import logoFileDk from "~/assets/images/logo/full-red-white.svg";
 
 
 interface Props {
-  action?: JSX.Element
+  location: string
 }
 
 
-export class Header extends React.Component<Props> {
+export const Header = (props: Props): JSX.Element => {
 
-  action = () => {
-    return (
-      <div className="col-span-1 rt">
-        {this.props.action}
-      </div>
-    );
+  const urlLogin = "/landing/login";
+
+  const history = RouterDOM.useHistory();
+
+  const login = () => {
+    history.push(urlLogin);
   };
 
-  render(): JSX.Element {
-    return (
-      <div className="p-4 grid grid-cols-2 gap-4" >
+  const button: JSX.Element = (() => {
+    if (props.location === urlLogin) {
+      return null;
+    }
+    else if (AuthUtils.isAuthenticated()) {
+      return (
+        <Logout />
+      );
+    }
+    else {
+      return (
+        <Button text="Login"
+                color="cardinal-inv"
+                callback={login} />
+      );
+    }
+  })();
+
+  return (
+    <div className="p-4 grid grid-cols-2 gap-4" >
+      <RouterDOM.Link to="/">
         <div className="col-span-1 mt-2">
           <img className="light:hidden h-8" src={logoFileDk} />
           <img className="dark:hidden h-8" src={logoFileLt} />
         </div>
-        {this.action()}
+      </RouterDOM.Link>
+      <div className="col-span-1 rt">
+        {button}
       </div>
-    );
-  }
+    </div>
+  );
 
-}
+};
--- src/components/landing/Main.tsx
+++ src/components/landing/Main.tsx
@@ -1,50 +1,27 @@
 import * as React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { RouteComponentProps} from "react-router";
 
-import * as AuthUtils from "~/src/utils/auth";
-import { Button } from "~/src/components/Button";
 import { Header } from "~/src/components/landing/Header";
 import { Home } from "~/src/components/landing/Home";
 import { Company } from "~/src/components/landing/Company";
 import { Login } from "~/src/components/landing/Login";
-import { Logout } from "~/src/components/Logout";
 import { NotFound as ErrorNotFound } from "~/src/components/errors/NotFound";
 
 
-export class Main extends React.Component<RouteComponentProps<void>, {}> {
+export const Main = (props: RouteComponentProps<void>) => {
 
-  headerButton = () => {
-    if (this.props.location.pathname === "/landing/login") {
-      return null;
-    }
-    else if (AuthUtils.isAuthenticated()) {
-      return (
-        <Logout />
-      );
-    }
-    else {
-      return (
-        <Button text="Login"
-                color="cardinal-inv"
-                callback={() => { return (<Redirect to="/landing/login" />); }} />
-      );
-    }
-  };
+  return (
+    <div>
+      <Header location={props.location.pathname} />
+      <Switch>
+        <Route exact path="/landing/" component={Home} />
+        <Route exact path="/landing/home" component={Home} />
+        <Route exact path="/landing/company" component={Company} />
+        <Route exact path="/landing/login" component={Login} />
+        <Route path="/landing/*" component={ErrorNotFound} />
+      </Switch>
+    </div>
+  );
 
-  render(): JSX.Element {
-    return (
-      <div>
-        <Header action={this.headerButton()} />
-        <Switch>
-          <Route exact path="/landing/" component={Home} />
-          <Route exact path="/landing/home" component={Home} />
-          <Route exact path="/landing/company" component={Company} />
-          <Route exact path="/landing/login" component={Login} />
-          <Route path="/landing/*" component={ErrorNotFound} />
-        </Switch>
-      </div>
-    );
-  }
-
-}
+};
