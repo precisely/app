2bb86e0ff7f0e5f14508bc133ba90ea95fc0f1f7
Constantine Vetoshev <constantine@precise.ly>
2020-07-29 13:15:19 -0700

    Style the Login component.

--- src/components/landing/Login.tsx
+++ src/components/landing/Login.tsx
@@ -4,21 +4,43 @@ import * as Router from "react-router";
 
 import * as AuthUtils from "~/src/utils/auth";
 import * as LoginUtils from "~/src/utils/login";
+import { Button } from "~/src/components/Button";
 
 import "./Login.css";
 
 
 export const Login = (props: Router.RouteComponentProps) => {
 
+  const loginFormRef = React.useRef(null);
+  const [valid, setValid] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   const login = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const email = event.target["email"].value;
-    const password = event.target["password"].value;
     await LoginUtils.login(email, password);
     const to = _.get(props, ["location", "state", "from"], "/");
     props.history.push(to);
   };
 
+  const onChangeEmail = (event: React.FormEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onChangePassword = (event: React.FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  React.useEffect(() => {
+    setValid(
+      loginFormRef.current && _.every(
+        loginFormRef.current.elements,
+        (elt: HTMLFormElement) => {
+          return elt.checkValidity();
+        }
+      )
+    );
+  });
 
   if (AuthUtils.isAuthenticated()) {
     return (
@@ -28,22 +50,41 @@ export const Login = (props: Router.RouteComponentProps) => {
 
   return (
     <div className="">
-      <form onSubmit={login}>
-        <div>
-          <label>Email</label>
-          <input className="input-text"
-                 type="email" name="email" />
-        </div>
-        <div className="">
-          <label>Password</label>
-          <input className="input-text"
-                 type="password" name="password" />
-        </div>
-        <div>
-          <button className="bg-blue-500 text-white"
-                  type="submit">
-            Login
-          </button>
+      <div className="pt-5 pb-5 flex justify-center text-lg font-bold">
+        Welcome Back to Precisely
+      </div>
+      <form ref={loginFormRef} onSubmit={login}>
+        <div className="pt-5 grid grid-cols-4 text-sm">
+          <div className="col-start-2 col-span-2 pb-8">
+            <label>
+              Email
+              <input className="input-text"
+                     placeholder="Enter your email"
+                     value={email}
+                     required
+                     pattern=".+@.+"
+                     onChange={onChangeEmail}
+                     name="email" />
+            </label>
+          </div>
+          <div className="col-start-2 col-span-2 pb-8">
+            <label>
+              Password
+              <input className="input-text"
+                     placeholder="Enter your password"
+                     value={password}
+                     onChange={onChangePassword}
+                     type="password"
+                     required
+                     name="password" />
+            </label>
+          </div>
+          <div className="col-start-2 col-span-2">
+            <Button type="submit"
+                    color={valid ? "cardinal" : "grey"}
+                    classes="w-full py-2"
+                    text="Login" />
+          </div>
         </div>
       </form>
     </div>
