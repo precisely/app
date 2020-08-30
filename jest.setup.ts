34e7cc1e3877c94fc59b6f9ca40aab9e264e279b
Constantine Vetoshev <constantine@precise.ly>
2020-08-30 00:17:48 -0700

    Add new testing dependencies.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import * as dotenv from "dotenv";
+// @ts-ignore
+import fetch from "node-fetch";
+
+
+// enable load of .env.test; required for correct test environment variable setup
+dotenv.config();
+
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+}
