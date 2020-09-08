88d48d46ce80ff990f33841500c27195482cac4f
Constantine Vetoshev <constantine@precise.ly>
2020-09-08 12:10:39 -0700

    Re-rig fetch testing.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,13 +1,7 @@
 import "@testing-library/jest-dom";
 import * as dotenv from "dotenv";
-// @ts-ignore
-import fetch from "node-fetch";
 
 
+// TODO: Review this. It may not be necessary.
 // enable load of .env.test; required for correct test environment variable setup
 dotenv.config();
-
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch;
-}
