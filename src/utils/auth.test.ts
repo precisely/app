1da817890f2307627ffa7f250a738ac911ccd724
Constantine Vetoshev <constantine@precise.ly>
2020-09-13 11:01:56 -0700

    Move mocks for (server) API calls around.

--- src/utils/auth.test.ts
+++ src/utils/auth.test.ts
@@ -1,4 +1,4 @@
-import * as Mock from "~/src/utils/mock";
+import * as MockAPI from "~/src/mocks/api";
 
 import * as AuthUtils from "./auth";
 import * as SessionUtils from "./session";
@@ -7,12 +7,12 @@ import * as SessionUtils from "./session";
 describe("AuthUtils", () => {
 
   test("login-logout sequence", async () => {
-    Mock.oneLoginAs("alice@example.com");
+    MockAPI.oneLoginAs("alice@example.com");
     expect(SessionUtils.isAuthenticated()).toBeFalsy();
     const res = await AuthUtils.login("alice@example.com", "password123");
     expect(res).toEqual(true);
     expect(SessionUtils.isAuthenticated()).toBeTruthy();
-    Mock.oneLogout();
+    MockAPI.oneLogout();
     await AuthUtils.logout();
     expect(SessionUtils.isAuthenticated()).toBeFalsy();
   });
