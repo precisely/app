837bace31ec405d59300ffa1e595081c643371d1
Constantine Vetoshev <constantine@precise.ly>
2020-10-01 20:18:31 -0700

    Add failed confirmation test.

--- src/components/landing/SignupConfirm.test.tsx
+++ src/components/landing/SignupConfirm.test.tsx
@@ -1,10 +1,7 @@
-import * as History from "history";
 import * as React from "react";
-import * as Router from "react-router";
 import * as TLR from "@testing-library/react";
 
 import * as MockAPI from "~/src/mocks/api";
-import * as MockSession from "~/src/mocks/session";
 import * as ReactUtils from "~/src/utils/react";
 import * as SessionUtils from "~/src/utils/session";
 
@@ -28,6 +25,19 @@ describe("SignupConfirm", () => {
     await TLR.waitFor(() => {
       expect(TLR.screen.getAllByText(/Confirmation/)[1])
 	.toHaveTextContent("Confirmation succeeded!");
+    });
+  });
+
+  test("failed confirm", async () => {
+    TLR.render(ReactUtils.routedComponent(
+      (props) => {
+	return <SignupConfirm {...props} />;
+      },
+      {route: "/landing/signup/confirm/:token", path: "/landing/signup/confirm/abcd1234"}
+    ));
+    await TLR.waitFor(() => {
+      expect(TLR.screen.getAllByText(/Confirmation/)[1])
+	.toHaveTextContent("Confirmation failed.");
     });
   });
 
