e9b449db5bdc851456e9cf899fa7e7f579e0c36f
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-12-08 13:38:10 -0800

    Pair programming WIP

--- src/components/app/pia/ChatChoices.tsx
+++ src/components/app/pia/ChatChoices.tsx
@@ -1,21 +1,42 @@
 import * as React from "react";
+import { ChatProps } from "~/src/components/app/pia/ChatProps";
 
-export interface Choice {
+/**
+ *
+ * Displays a set of choices as buttons (for now)
+ * We may include styles of choice in the future.
+ *
+ */
+export interface ChoiceItemProps {
   id: string,
   text: string
+  runId: string
 }
 
-export interface ChatChoicesProps {
+export interface ChatChoicesProps extends ChatProps {
   type: "choices",
-  choices: Choice[],
+  style: "buttons" | "list",
+  choices: ChoiceItemProps[],
   text: string
 }
+
+function clickCallback(runId: string, choiceId: string) {
+  return () => <div>Call pia-server/continue with for run {runId} and choice {choiceId}</div>;
+}
+
+export const ChoiceItem = (props: ChoiceItemProps): JSX.Element =>
+  <button
+    className="btn"
+    onClick={clickCallback(props.runId, props.id)}>
+    {props.text}
+  </button>;
+
 
 export const ChatChoices = (props: ChatChoicesProps) => {
   return (
-    <div>
-      {props.text}
-      {props.choices}
+    <div id={"choice_" + props.id}>
+      <div>{props.text}</div>
+      {... props.choices.map(c => ChoiceItem(c))}
     </div>
   );
 }
---
+++ src/components/app/pia/ChatProps.tsx
@@ -0,0 +1,4 @@
+export interface ChatProps {
+  id: string,
+  type: string
+}
