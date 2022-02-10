1c80e5fca0ca306798e94839641dd4d0c6985203
Aneil Mallavarapu <aneilmallavar@gmail.com>
2022-02-10 00:28:33 -0800

    Move clinic button to the top

--- src/components/demo/Main.tsx
+++ src/components/demo/Main.tsx
@@ -32,6 +32,14 @@ export const Main = () => {
           Demo Driver
         </div>
       </div>
+      <div className="pt-6 grid grid-cols-3">
+        <div className="col-start-2">
+          <Button text="Clinic UI"
+                  color="cardinal"
+                  classes="w-full py-2"
+                  callback={() => history.push("/demo/clinic")} />
+        </div>
+      </div>
       <div className="pt-6 grid grid-cols-6">
         <div className="col-start-3 col-span-1 pr-2">
           <input className="w-full h-full"
@@ -45,14 +53,6 @@ export const Main = () => {
                   color="cardinal"
                   classes="w-full py-2 pr-2"
                   callback={() => history.push(`/demo/patient/${patientId}`)} />
-        </div>
-      </div>
-      <div className="pt-6 grid grid-cols-3">
-        <div className="col-start-2">
-          <Button text="Clinic UI"
-                  color="cardinal"
-                  classes="w-full py-2"
-                  callback={() => history.push("/demo/clinic")} />
         </div>
       </div>
       <div className="pt-6 grid grid-cols-6">
